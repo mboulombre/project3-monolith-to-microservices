92a234f24874c70df99c289b8693090f2deee7e3
De-Dieu @hidden <mboulmbr@gmail.com>
2022-10-01 04:42:55 +0100

    update pipeline

--- set_env.sh
+++ set_env.sh
@@ -1,14 +1,4 @@
-# This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-# export POSTGRES_USERNAME=databasedb
-# export POSTGRES_PASSWORD=databasedb
-# export POSTGRES_HOST=database-udagram.cexxjxhssacy.us-east-1.rds.amazonaws.com
-# export POSTGRES_DB=databasedb
-# export AWS_BUCKET=udagram275488887175
-# export AWS_REGION=us-east-1
-# export AWS_PROFILE=default
-# export JWT_SECRET=testing
-# export URL=http://localhost:8100
+
 export POSTGRES_USERNAME=databasedb
 export POSTGRES_PASSWORD=databasedb
 export POSTGRES_HOST=databasedb.cl9ojkplzhb5.us-east-1.rds.amazonaws.com
