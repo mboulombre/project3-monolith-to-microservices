35fe16a771fa4224b182f2ad8ee9716a336cff3d
De-Dieu @hidden <mboulmbr@gmail.com>
2022-10-05 18:56:17 +0100

    update frontend v3

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   // apiHost: 'http://localhost:8080/api/v0' 666
-  apiHost: 'http://a9086d20380f0498fb7fa5e00f913d8f-149419741.us-east-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a62913af039984848b26f93d2f92b968-1304374342.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
